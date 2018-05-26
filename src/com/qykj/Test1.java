61dc04c6d31bdcdc767831494f8c6addc722a188
BOBO <BOBO@LAPTOP-UE6KL0KI>
2018-05-27 00:15:56 +0800

    jackson 测试

--- src/com/qykj/Test1.java
+++ src/com/qykj/Test1.java
@@ -1,8 +1,26 @@
 package com.qykj;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Test1 {
-	public static void main(String args[])
-	{
+	public static void main(String args[]) throws ParseException, JsonProcessingException {
 		System.out.println("hello,word!");
+
+		 
+		User user = new User();
+		user.setName("hello,word!");
+		user.setEmail("abcd@163.com");
+		user.setAge(20);
+
+		SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+		user.setBirthday(dateformat.parse("2018-10-01"));
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(user);
+		System.out.println(json);
+
 	}
 }
